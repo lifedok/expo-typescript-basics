cd8ab20fa18bfb8b448fceedee12fd8c39f48b32
aleksei.b <aleksei.b@adngames.com>
2023-12-27 23:14:36 +0200

    add real data

--- App.tsx
+++ App.tsx
@@ -1,13 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Welcome from "./components/welcome";
-
+import { getPokemonList } from "./services/user-service/user.service";
+import { useEffect, useState } from "react";
+// npx sb@latest init --type react_native
 export default function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // fetch data
+    const dataFetch = async () => {
+      const data = await getPokemonList();
+      // set state when the data received
+      setPosts(data);
+    };
+
+    dataFetch();
+  }, []);
+  console.log('pokemonList', posts);
   return (
     <View style={styles.container}>
+
       <Welcome/>
       {/*<Text>Open up App.tsx to start working on your app!</Text>*/}
-      {/*<StatusBar style="auto" />*/}
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -15,7 +31,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'mediumslateblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
