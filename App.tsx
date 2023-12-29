e7cfbfc0d46a133dadcd51c4d50ef37ad0798e79
aleksei.b <aleksei.b@adngames.com>
2023-12-29 11:34:49 +0200

    add mobx and mobx-react

--- App.tsx
+++ App.tsx
@@ -1,74 +1,16 @@
 import React, { Component } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Welcome from "./src/components/welcome";
-import { getPokemonList } from "./src/services/user-service/user.service";
-import { useEffect, useState } from "react";
 import Constants from "expo-constants";
 import Storybook from "./.storybook";
-// import { MenuProvider } from 'react-native-popup-menu';
-
-// export default class App extends Component<{}> {
-//   render() {
-//     return (
-//       <MenuProvider backHandler={true}>
-//         <View
-//           style={{ flex:1 }}>
-//           <StatusBar barStyle="light-content" backgroundColor="rgba(222, 88, 51, 1)"/>
-//           <Provider store={store}>
-//             <MainAppNavigator/>
-//           </Provider>
-//         </View>
-//       </MenuProvider>
-//     );
-//   }
-// }
-
-
-// function App() {
-//   const [posts, setPosts] = useState([]);
-//
-//   useEffect(() => {
-//     // fetch data
-//     const dataFetch = async () => {
-//       const data = await getPokemonList();
-//       // set state when the data received
-//       setPosts(data);
-//     };
-//
-//     dataFetch();
-//   }, []);
-//   console.log('pokemonList', posts);
-//
-//   return (
-//     <View style={styles.container}>
-//
-//       <Welcome/>
-//       {/*<Text>Open up App.tsx to start working on your app!</Text>*/}
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
+import { ListPage } from "./src/pages/list-page/list-page";
 
 class App extends Component<{}, {}> {
-  private data = {}
-
-  // @action
-  // setIsShuffledPoolsLoading(key: PoolFilterKeys, value: boolean) {
-  //   this.isShuffledPoolsLoading[key] = value;
-  // }
-  //
-  componentDidMount() {
-    // this.data = await getPokemonList()
-  }
 
   render() {
-    console.log('data', this.data);
     return (
       <View style={styles.container}>
-
-        <Welcome/>
-        {/*<Text>Open up App.tsx to start working on your app!</Text>*/}
+        <ListPage/>
         <StatusBar style="auto" />
       </View>
     );
